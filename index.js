b456608660545ee389631bc3c930b4a5bb173012
Sean Derrow <sderrow@mit.edu>
2018-01-20 16:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,12 +84,14 @@ function createRock(x) {
      * we should remove the rock from the DOM
      */
      if (checkCollision(rock)) {
+       console.log("Ended game")
        endGame()
      } else if (top < GAME_HEIGHT) {
-       console.log("Got in the right if-section")
+       console.log("Moved down")
        top += 2
        rock.style.top = `${top}px`
      } else {
+       console.log("Removed rock")
        rock.remove()
      }
   }
