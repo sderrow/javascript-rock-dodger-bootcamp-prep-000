12e0454ed073cf797ab98faf15ce05cae766a42a
Sean Derrow <sderrow@mit.edu>
2018-01-20 15:39:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,17 +38,7 @@ function checkCollision(rock) {
 
     if (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge ||
         rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge ||
-        rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge
-        /**
-               * Think about it -- what's happening here?
-               * There's been a collision if one of three things is true:
-               * 1. The rock's left edge is < the DODGER's left edge,
-               *    and the rock's right edge is > the DODGER's left edge;
-               * 2. The rock's left edge is > the DODGER's left edge,
-               *    and the rock's right edge is < the DODGER's right edge;
-               * 3. The rock's left edge is < the DODGER's right edge,
-               *    and the rock's right edge is > the DODGER's right edge
-               */) {
+        rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge) {
       return true
     }
   }
