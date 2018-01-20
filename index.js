0c8c363ac629470bc914fbf3a848bfe2b0bad6bf
Sean Derrow <sderrow@mit.edu>
2018-01-20 16:34:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,13 +83,14 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
+     top += 2
+     rock.style.top = `${top}px`
      if (checkCollision(rock)) {
        console.log("Ended game")
        endGame()
      } else if (top < GAME_HEIGHT) {
        console.log("Moved down")
-       top += 2
-       rock.style.top = `${top}px`
+       window.requestAnimationFrame(moveRock)
      } else {
        console.log("Removed rock")
        rock.remove()
